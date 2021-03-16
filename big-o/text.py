ddcf9248c4cce0ddcd353f4d4f4489a86fcc5cfb
Carlos Panganiban <cgpanganiban@up.edu.ph>
2021-03-17 03:52:32 +0800

    Add code from Algorithmic Efficiency (https://youtu.be/48QSumLWJJA)

--- big-o/text.py
+++ big-o/text.py
@@ -1,21 +1,9 @@
-# This is the file of the current scene being worked on.
-# Scenes are then archived in their respective episode folders.
-
 from manim import *
-from classes import text
-from random import randint
 
-config.background_color = "#0d121f"
-config.max_files_cached = 1000
-
-class Video(Scene):
-    def construct(self):
-        text_list = [
-            # r"Let's count how many times the sorting algorithm\\ does \textbf{comparisons}.",
-            # r"An operation by an algorithm is \textbf{NOT} free.",
-            # r"This includes accessing and manipulating variables,\\ doing comparisons, etc.",
-            # r"Each operation takes up \textbf{time}.",
-            # r"So, what arrangement of numbers would lead\\ to the \textbf{least} number of checks?"
-            r"Then, what arrangement of numbers would lead\\ to the \textbf{most} number of checks?"
-        ]
-        text.transition_all(self, text_list, transition_time=1.0)
+def transition_all(scene, text_list, transition_time=1.0):
+    first_tex = Tex(text_list[0])
+    scene.play(Write(first_tex), run_time=transition_time)
+    scene.wait()
+    for text in text_list[1:]:
+        scene.play(Transform(first_tex, Tex(text)), run_time=transition_time)
+        scene.wait()
--- classes/text.py
+++
@@ -1,9 +0,0 @@
-from manim import *
-
-def transition_all(scene, text_list, transition_time=1.0):
-    first_tex = Tex(text_list[0])
-    scene.play(Write(first_tex), run_time=transition_time)
-    scene.wait()
-    for text in text_list[1:]:
-        scene.play(Transform(first_tex, Tex(text)), run_time=transition_time)
-        scene.wait()
